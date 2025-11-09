2218a99c627208df5ede1df681d489242b0c68a6
IOSF <om2figueroa@gmail.com>
2025-11-09 13:53:22 +0100

    COMENT TEMPORALLY FETCH

--- src/api/apiFetch.js
+++ src/api/apiFetch.js
@@ -1,6 +1,6 @@
 import {apiConfig} from './apiConfig.js';
 
-
+/*
 
 //------------------------------------
 //          FETCH    SELECT  
@@ -17,7 +17,7 @@ selector.addEventListener("change", event =>{
     if(event.target.value===""){
         return;
     }else {
-        getData(divDinamic/*¿esto seria el anchorElement#app?*/ ,event.target.value/*value del select!*/)
+        getData(divDinamic¿esto seria el anchorElement#app? ,event.target.valuevalue del select!)
     }
 })
 
@@ -34,17 +34,17 @@ export async function getData(dondeVa, categoria="popular")
 
         }////si no hay res lanza error
         const data = await res.json();//////data es igual a res en formato JSON//
-        /*showMovies(data.results, container); ME FALTARIA ALGO COMO UNA FUNCION CREATE_CARDMOVIE -TODO-TODO-TODO-*/
+        ////showMovies(data.results, container); ME FALTARIA ALGO COMO UNA FUNCION CREATE_CARDMOVIE -TODO-TODO-TODO-
         return mostrarContenido(data, dondeVa);///AHORA LA DATA LLAMA A LA FUNCION MOSTRAR CONTENIDO
 
-        /* const divP = document.createElement("p");////creame un "P"
+        ///// const divP = document.createElement("p");////creame un "P"
         divP.textContent = JSON.stringify(data, null, 2);////el contenido de ese divP es el data en formato.stringify
         div.appendChild(divP);/////appendChild de ese divP 
-        */
+        ////////////
     } catch (error) {
         console.error("Error en la petición:", error.message);
     }
 }
-
+*/
 //fetch------------------------------------------------->
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
